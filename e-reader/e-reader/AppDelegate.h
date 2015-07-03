a835539909a0384d3376eb5bb0ad986c47a4b5b0
cabbage <570262616@qq.com>
2015-07-03 21:53:00 +0800

    new
    易阅读创建项目

---
+++ e-reader/e-reader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  e-reader
+//
+//  Created by 张鹏 on 15/7/3.
+//  Copyright © 2015年 smallCobblerStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ e-reader/e-reader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  e-reader
+//
+//  Created by 张鹏 on 15/7/3.
+//  Copyright © 2015年 smallCobblerStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
